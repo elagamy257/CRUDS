230a8c5b7a54e1b8dcd230058cd73ad446d0c816
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-07-02 22:12:35 +0300

    Update crud.js

--- cruds/crud.js
+++ cruds/crud.js
@@ -8,7 +8,6 @@ let count=document.getElementById('count')
 let category=document.getElementById('category');
 let submit=document.getElementById('submit');
 
-// console.log(title,discount,price,taxes,ads,total,count,category,submit);
 // get total
 function getTotal(){
     // console.log('done');
@@ -24,7 +23,6 @@ function getTotal(){
 }
 
 // create product
-
 let dataPro;
 if(localStorage.product !=null)
     dataPro=JSON.parse(localStorage.product)
@@ -42,13 +40,78 @@ submit.onclick=function(){
         category:category.value
     }
     // console.log(newPro);
-    dataPro.push(newPro);
-    localStorage.setItem('product', JSON.stringify(dataPro) )
-    console.log(newPro);
-}
-
-
+    if(newPro.count>1){
+        for (let i = 0; i <newPro.count; i++) {
+            dataPro.push(newPro);
+        }
+    }else{
+        dataPro.push(newPro);
+    }
+    
 
 // save localStorage
+    localStorage.setItem('product', JSON.stringify(dataPro) )
+    // console.log(newPro);
+
+    clearData();
+    showData();
+}
+
 // clear input
+function clearData(){
+        title.value='';
+        price.value='';
+        taxes.value='';
+        ads.value='';
+        discount.value='';
+        total.innerHTML='';
+        count.value='';
+        category.value='';
+}
+
 //read
+function showData(){
+    let table='';
+    for (let i = 0; i < dataPro.length; i++) {
+        table +=`
+        <tr>
+        <td>${i}</td>
+        <td>${dataPro[i].title}</td>
+        <td>${dataPro[i].price}</td>
+        <td>${dataPro[i].taxes}</td>
+        <td>${dataPro[i].ads}</td>
+        <td>${dataPro[i].discount}</td>
+        <td>${dataPro[i].total}</td>
+        <td>${dataPro[i].category}</td>
+        <td><button id="update">update</button></td>
+        <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+        </tr>`;
+        
+        
+    }
+
+    document.getElementById('tbody').innerHTML=table;
+    let btnDelete=document.getElementById('deleteAll');
+    if(dataPro.length>0){
+        btnDelete.innerHTML=`
+        <button onclick="deleteAll()"> delete All (${dataPro.length})</button>
+        `
+    }else{
+        btnDelete.innerHTML=''
+    }
+}
+
+//delete
+function deleteData(i){
+    dataPro.splice(i,1);
+    localStorage.product=JSON.stringify(dataPro);
+    showData();
+}
+
+//delete All
+function deleteAll(){
+    localStorage.clear()
+    dataPro.splice(0)
+    showData();
+
+}
